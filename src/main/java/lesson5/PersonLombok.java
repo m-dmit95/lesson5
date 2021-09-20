788de67453d866fb7e1e805108aba6cc5dd4c99f
m-dmit95 <m.dmit95@gmail.com>
2021-09-21 00:46:01 +0700

    fixed PersonLombok

--- src/main/java/lesson5/PersonLombok.java
+++ src/main/java/lesson5/PersonLombok.java
@@ -1,8 +1,12 @@
 package lesson5;
 
-import lombok.AllArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class PersonLombok {
     private String fio;
     private String position;
@@ -10,35 +14,24 @@ public class PersonLombok {
     private String phone;
     private int salary;
     private int age;
-
-    public void info() {
-        System.out.println(
-                "Сотрудник: " + fio +
-                        ", Должность: " + position +
-                        ", E-Mail: " + email +
-                        ", Телефон: " + phone +
-                        ", Зарплата: " + salary +
-                        ", Возраст: " + age
-        );
-    }
 }
 
 class MainClassByLombok {
     public static void main(String[] args) {
-        Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
-        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
-        persArray[1] = new Person("Pupkin Vasiliy", "Manager", "vasya@mailbox.com", "892311111", 40000, 25);
-        persArray[2] = new Person("Sergeyev Sergey", "Security", "sergey@mailbox.com", "892322222", 20000, 40);
-        persArray[3] = new Person("Erokhin Ivan", "CEO", "erokhin@mailbox.com", "892333333", 300000, 45);
-        persArray[4] = new Person("Sokolov Vladimir", "QA", "sokol@mailbox.com", "892344444", 25000, 20);
+        PersonLombok[] persArray = new PersonLombok[5]; // Вначале объявляем массив объектов
+        persArray[0] = new PersonLombok("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        persArray[1] = new PersonLombok("Pupkin Vasiliy", "Manager", "vasya@mailbox.com", "892311111", 40000, 25);
+        persArray[2] = new PersonLombok("Sergeyev Sergey", "Security", "sergey@mailbox.com", "892322222", 20000, 40);
+        persArray[3] = new PersonLombok("Erokhin Ivan", "CEO", "erokhin@mailbox.com", "892333333", 300000, 45);
+        persArray[4] = new PersonLombok("Sokolov Vladimir", "QA", "sokol@mailbox.com", "892344444", 25000, 20);
 
-        for (Person person : persArray) {
+        for (PersonLombok person : persArray) {
             if (person.getAge() >= 40) {
-                person.info();
+                System.out.println(person.toString());
             }
         }
 
-        Person personBySetter = new Person();
+        PersonLombok personBySetter = new PersonLombok();
         personBySetter.setFio("Vladimirov Semyon");
         personBySetter.setPosition("Janitor");
         personBySetter.setEmail("semyon@mailbox.com");
@@ -46,6 +39,13 @@ class MainClassByLombok {
         personBySetter.setSalary(15000);
         personBySetter.setAge(50);
 
-        personBySetter.info();
+        System.out.println(personBySetter.toString());
+
+        System.out.println(personBySetter.getFio());
+        System.out.println(personBySetter.getPosition());
+        System.out.println(personBySetter.getEmail());
+        System.out.println(personBySetter.getPhone());
+        System.out.println(personBySetter.getSalary());
+        System.out.println(personBySetter.getAge());
     }
 }
