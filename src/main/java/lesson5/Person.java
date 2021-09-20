d59539cca8e3e7cc3b379aa7a87275ed7c427075
m-dmit95 <m.dmit95@gmail.com>
2021-09-20 23:44:58 +0700

    added Person

--- src/main/java/lesson5/Person.java
+++ src/main/java/lesson5/Person.java
@@ -17,19 +17,66 @@ public class Person {
         this.age = age;
     }
 
+    public Person() {
+    }
+
     public void info() {
         System.out.println(
                 "Сотрудник: " + fio +
-                ", Должность: " + position +
-                ", E-Mail: " + email +
-                ", Телефон: " + phone +
-                ", Зарплата: " + salary +
-                ", Возраст: " + age
+                        ", Должность: " + position +
+                        ", E-Mail: " + email +
+                        ", Телефон: " + phone +
+                        ", Зарплата: " + salary +
+                        ", Возраст: " + age
         );
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 
     public int getAge() {
         return age;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 
@@ -42,11 +89,20 @@ class MainClass {
         persArray[3] = new Person("Erokhin Ivan", "CEO", "erokhin@mailbox.com", "892333333", 300000, 45);
         persArray[4] = new Person("Sokolov Vladimir", "QA", "sokol@mailbox.com", "892344444", 25000, 20);
 
-
         for (Person person : persArray) {
             if (person.getAge() >= 40) {
                 person.info();
             }
         }
+
+        Person personBySetter = new Person();
+        personBySetter.setFio("Vladimirov Semyon");
+        personBySetter.setPosition("Janitor");
+        personBySetter.setEmail("semyon@mailbox.com");
+        personBySetter.setPhone("8(123)123-45-67");
+        personBySetter.setSalary(15000);
+        personBySetter.setAge(50);
+
+        personBySetter.info();
     }
 }
